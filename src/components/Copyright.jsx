32328b32d4a95eeba116948733dbf9ced332f39a
David Finster <david@fortc.com>
2022-06-11 10:16:19 -0400

    Update copyright

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -6,7 +6,7 @@ function Copyright(props) {
         return (
             <>
             <Typography variant="caption" display="block" align="center" gutterBottom>
-            <a href="/about">Lultr.com &copy; David Finster</a>
+            <a href="/about">&copy; David Finster</a>
             <br />
             <a href="/about">About</a> &bull; <a href="/privacy">Privacy</a> &bull; <a href="/terms">Terms</a> &bull; <a href="https://github.com/dfinr/lultr">GitHub</a>
             </Typography>
