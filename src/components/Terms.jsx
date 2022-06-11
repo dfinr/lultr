63a634d865a2932295354c3f4d630cb001a06ba2
David Finster <david@fortc.com>
2022-06-11 09:26:35 -0400

    3.0.3

---
+++ src/components/Terms.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Button, CssBaseline, Typography, Box } from '@mui/material/';
+import { FaHome, FaTwitter, FaEnvelope } from 'react-icons/fa';
+import Copyright from "./Copyright";
+
+
+function Terms() {
+
+    return (
+        <>
+            <CssBaseline />
+            <Box sx={{ width: '90%', maxWidth: 800, mt: 2, display: 'flex', flexDirection: 'column', margin: 'auto' }} >
+            <Box sx={{ display: 'flex', margin: '1rem auto 1rem', justifyContent: 'center', flexDirection: 'row', flexWrap: 'wrap' }} >
+                <Button sx={{ margin: '0px 6px 0px 6px' }} variant="outlined" startIcon={<FaHome />} 
+                    href="/">
+                        Home
+                </Button>
+                <Button sx={{ margin: '0px 6px 0px 6px' }} variant="outlined" startIcon={<FaTwitter />} href="https://twitter.com/LultrCom">
+                        @LultrCom
+                </Button>
+                <Button sx={{ margin: '0px 6px 0px 6px' }} variant="outlined" startIcon={<FaEnvelope />} href="mailto:lultr@pm.me">
+                        lultr@pm.me
+                </Button>
+            </Box>
+            <Box sx={{ display: 'flex', margin: '2rem auto 4rem', justifyContent: 'center', flexDirection: 'column', flexWrap: 'wrap' }} >
+                <Typography paragraph variant="h1" align="center">
+                    Terms of Service
+                </Typography>
+                <Typography paragraph sx={{ textAlign: 'left' }} variant="body2" gutterBottom>
+                    You may use this site to view, email, tweet, and copy jokes. We provide links to credit when known. It's polite to credit the original author when you copy jokes from this site.
+                </Typography>
+                </Box>
+                <Copyright />
+            </Box>
+        </>
+    );
+}
+
+export default Terms;
